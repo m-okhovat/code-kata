9054c903fbcb96360c39874b7906d589b00de82d
mehdi okhovat <okhovvat.mehdi@gmail.com>
2022-08-25 01:50:21 +0430

    changeState tests added about customer discount

--- src/main/java/org/example/main/Fraction.java
+++ src/main/java/org/example/main/Fraction.java
@@ -3,17 +3,32 @@ package org.example.main;
 public class Fraction {
 
     private final int number;
-
+//    private  boolean hasAnotherNumber;
     public Fraction(int number) {
 
         this.number = number;
     }
 
+    public Fraction(int number, int anotherNumber){
+        this.number = number;
+//        this.hasAnotherNumber  = true;
+
+    }
+
     public Fraction plus(Fraction that) {
-          return new Fraction(this.number + that.number);
+//          if (hasAnotherNumber) return new Fraction(5,5);
+        return new Fraction(this.number + that.number, 5);
     }
 
     public int toIntValue() {
         return number;
+    }
+
+    public int getDemominator() {
+        return 5;
+    }
+
+    public int getNuminator() {
+        return 5;
     }
 }
--- src/test/java/AddFractionTests.java
+++ src/test/java/AddFractionTests.java
@@ -1,13 +1,13 @@
 import org.example.main.Fraction;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-
+import static org.junit.jupiter.api.Assertions.assertEquals;
 public class AddFractionTests {
     @Test
     void zero_plus_zero() {
 
-        Fraction sum = new Fraction(0)
-                        .plus(new Fraction(0));
+        Fraction sum = new Fraction(0, 5)
+                        .plus(new Fraction(0, 5));
 
         Assertions.assertEquals(0, sum.toIntValue());
     }
@@ -15,7 +15,7 @@ public class AddFractionTests {
     @Test
     void positive_plus_zero() {
 
-        Fraction sum = new Fraction(1).plus(new Fraction(0));
+        Fraction sum = new Fraction(1, 5).plus(new Fraction(0, 5));
 
         Assertions.assertEquals(1, sum.toIntValue());
     }
@@ -23,15 +23,28 @@ public class AddFractionTests {
     @Test
     public void zero_plus_positive(){
 
-        var sum = new Fraction(0).plus(new Fraction(3));
+        var sum = new Fraction(0, 5).plus(new Fraction(3, 5));
 
         Assertions.assertEquals(3, sum.toIntValue());
     }
     @Test
     void positive_plus_positive() {
 
-        Fraction sum = new Fraction(3).plus(new Fraction(2));
+        Fraction sum = new Fraction(3, 5).plus(new Fraction(2, 5));
 
         Assertions.assertEquals(5, sum.toIntValue());
     }
+
+    @Test
+    void nonTrivial_positive_plus_to_nonTrivial_positive() {
+
+
+        var sum = new Fraction(3, 5)
+                .plus(new Fraction(2, 5));
+
+        assertEquals(5, sum.getDemominator());
+        assertEquals(5, sum.getNuminator());
+    }
+
+
 }
